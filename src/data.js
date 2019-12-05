de06ef18f7480bbed0a3c446dd786be8e88c8045
kaarenzu <karenzcortes0211@gmail.com>
2019-12-05 13:34:55 -0300

    Agregamos funciones de Casas, estudiantes y staff

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const functionGryffindor = (POTTER) => {
  const filtrarGryffindor = POTTER.filter(POTTER => (POTTER.house=== "Gryffindor" ));
  
  console.log (filtrarGryffindor);
-  return 'functionData';
+  return 'functionGryffindor';
 };
 // Funcion que muestra los jugadores por casa(Slytherin)
 export const functionSlytherin = (POTTER) => {
@@ -12,8 +12,26 @@ export const functionSlytherin = (POTTER) => {
 const filtrarSlytherin = POTTER.filter(POTTER => (POTTER.house=== "Slytherin" ));
 console.log(filtrarSlytherin);
 };
-
-
+// Funcion que muestra los jugadores por casa(Ravenclaw)
+export const functionRavenclaw = (POTTER) =>{
+  const filtrarRavenclaw= POTTER.filter(POTTER =>(POTTER.house==="Ravenclaw"));
+  console.log(filtrarRavenclaw);
+};
+// Funcion que muestra los jugadores por casa(Hufflepuff)
+export const functionHufflepuff= (POTTER) => {
+  const filtrarHufflepuff= POTTER.filter(POTTER =>(POTTER.house==="Hufflepuff"));
+  console.log(filtrarHufflepuff);
+};
+//Funcion que muestra los jugadores que sean estudiantes 
+export const functionStudent = (POTTER) => {
+  const filtrarStudent= POTTER.filter(POTTER =>(POTTER.hogwartsStudent=== true));
+  console.log(filtrarStudent);
+};
+// Funcion que muestra a los jugadores que sean del staff 
+export const functionStaff = (POTTER) => {
+  const filtrarStaff=POTTER.filter(POTTER=>(POTTER.hogwartsStaff===true));
+  console.log(filtrarStaff);
+}
 
 
 
