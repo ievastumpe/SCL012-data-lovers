0478c612272d9b89ff88c17b46b7af7ea4c4c8a2
kaarenzu <karenzcortes0211@gmail.com>
2019-12-11 10:59:24 -0300

    cambios en el test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,81 +1,13 @@
 // importamos la función `example`
-import { functionGryffindor } from "../src/data";
-Gryffindor[
-  {
-    name: 'Harry Potter',
-    species: 'human',
-    gender: 'male',
-    house: 'Gryffindor',
-  
-  },
-  {
-    name: 'Hermione Granger',
-    species: 'human',
-    gender: 'female',
-    house: 'Gryffindor',
-  
-  },
-  {
-    name: 'Ron Weasley',
-    species: 'human',
-    gender: 'male',
-    house: 'Gryffindor',
-  
-  },
-  {
-    name: 'Minerva McGonagall',
-    species: 'human',
-    gender: 'female',
-    house: 'Gryffindor',
-  
-  },
-  {
-    name: 'Rubeus Hagrid',
-    species: 'half-giant',
-    gender: 'male',
-    house: 'Gryffindor',
-  },
-  {
-    name: 'Neville Longbottom',
-    species: 'human',
-    gender: 'male',
-    house: 'Gryffindor',
-   
-  },
-  {
-    name: 'Ginny Weasley',
-    gender: 'female',
-    house: 'Gryffindor',
-  },
-  {
-    name: 'Sirius Black',
-    species: 'human',
-    gender: 'male',
-    house: 'Gryffindor',
-  },
-  {
-    name: 'Remus Lupin',
-    species: 'werewolf',
-    house: 'Gryffindor',
- 
-  },
-  {
-    name: 'Arthur Weasley',
-    species: 'human',
-    house: 'Gryffindor',
-    
-  }
+import { example } from "../src/data";
 
-];
-
-
-describe('functionGryffindor', () => {
+describe('example', () => {
 
   it('debería ser una función', () => {
-    expect(typeof functionGryffindor).toBe('function');
+    expect(typeof example).toBe('function');
   });
 
-  describe('functionGryffindor', (Gryffindor) => {
+  describe('example', () => {
     // escribe aquí tu test
   });
 });
